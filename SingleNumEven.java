ffe5d8d11700c775103ce85e78974b7b2023d60f
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 14:45:38 +0530

    To find unique element in even times repeating numbers

---
+++ SingleNumEven.java
@@ -0,0 +1,17 @@
+//youre given a array of no. and if in that array every no appears twice only one no. appears once youve to find that no.
+//using xor coz a^a = 0 and 0^a = a
+public class SingleNumEven {
+    public static void main(String[] args) {
+        int[] arr={2,45,4,4,2,5,6,5,6};
+        System.out.println(unique(arr));
+    }
+
+    private static int unique(int[] arr) {
+        int ans =0;// becoz 0^a=a
+     for (int i : arr) {
+        ans^=i;
+     }
+     return ans;
+    
+    }
+}
