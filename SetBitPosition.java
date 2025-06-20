5a4b548b2bcc209671f5288ec5f1488afbc1d9e7
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 15:15:50 +0530

    Position of rightmost set bit

---
+++ SetBitPosition.java
@@ -0,0 +1,14 @@
+// to find the position of rightmost set bit 
+public class SetBitPosition {
+    public static void main(String[] args) {
+        int num = 36;
+        String binary = Integer.toBinaryString(num);
+        System.out.println("Binary: " + binary);
+
+        int value = num & -num;
+        System.out.println("Rightmost set bit value: " + value);
+
+        int position = Integer.numberOfTrailingZeros(value) + 1;
+        System.out.println("Position (1-based): " + position);
+    }
+}
