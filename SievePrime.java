deb34af5f78d040e9cbdcaf971086504117f4240
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 17:27:14 +0530

    Sieve method to find prime numbers

---
+++ SievePrime.java
@@ -0,0 +1,25 @@
+//sieve of eratosthenes is a method by which we can efficiently find the prime number via excluding the unwanted factor of the initial iterators and hence increasing the efficiency..
+public class SievePrime {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n + 1];
+        sieve(n, primes);
+    }
+
+    private static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = 2 * i; j <= n; j += i) {
+                    primes[j] = true; // mark as not prime
+                }
+            }
+        }
+
+        // Print all primesclear
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+}
+}
