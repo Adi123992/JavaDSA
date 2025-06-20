07e32047d32079b6b254060303d4ca193421a234
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 12:40:17 +0530

    Is a number Power of 2

---
+++ PowerTwo.java
@@ -0,0 +1,14 @@
+//You are given a no. find out if its power of 2 or not
+public class PowerTwo {
+    public static void main(String[] args) {
+        int num = 36;
+        String binary = Integer.toBinaryString(num);
+        System.out.println(binary);
+        if ((num&(num-1))==0) {
+            System.out.println("It is a power of 2");
+        }
+        else{
+        System.out.println("Not a power of 2");
+        }
+    }
+}
