501a077296cd3dce8c1d60a022cc528f45f8c285
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 14:31:09 +0530

    To find the Magic Number

---
+++ MagicNumber.java
@@ -0,0 +1,15 @@
+// to find the magic number via the binary form of the number and multiplying the 5 power according to the digit place
+public class MagicNumber {
+    public static void main(String[] args) {
+        int n = 6;
+        int ans=0;
+        int base=5;
+        while (n>0) {
+            int last = n&1;
+            ans+=last*base;
+            base = base*5;
+            n=n>>1; 
+        }
+        System.out.println("The Magic Number is : "+ans);
+    }
+}
