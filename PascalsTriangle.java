0b8e1bfa24eab77a9df37761e8f84713d6ac6508
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 14:25:02 +0530

    To get sum of nth row in Pascals Triangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,8 @@
+//find the sum of nth row
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        int n = 4;
+        int sum = 1<<n-1;
+        System.out.println("Sum of "+n+"th row is : "+sum);
+    }
+}
