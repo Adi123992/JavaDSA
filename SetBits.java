d20e0f7df1e73385972bb0cd5794c364f4e47b17
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-20 12:34:26 +0530

    No. of Set bits in a numbetr

---
+++ SetBits.java
@@ -0,0 +1,16 @@
+public class SetBits {
+    public static void main(String[] args) {
+        int num = 45;
+        //to see the binary representation
+        String binary = Integer.toBinaryString(num);
+        System.out.println(binary);
+        int count = 0;
+        while (num>0) {
+            if ((num&1)==1) {
+                count++;
+            }
+            num=num>>1;
+        }
+        System.out.println("Number of Set Bits i.e. (1's) :" +count );
+    }
+}
