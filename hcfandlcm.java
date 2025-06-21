e603f4176ed049ac95c139e9cafc788554154019
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-21 15:53:27 +0530

    HCF and LCM via euclidean algorithm

---
+++ hcfandlcm.java
@@ -0,0 +1,21 @@
+public class hcfandlcm {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 18;
+        System.out.println(gcd(a, b));
+        System.out.println(lcm(a, b));
+    }
+
+    // via euclidean algorithm
+    private static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+
+    private static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+}
