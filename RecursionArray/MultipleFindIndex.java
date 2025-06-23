6c8466eeb834bb7c2facb486baec646e8a630dde
addie <aditya.gumgaonkar9021@gmail.com>
2025-06-23 12:09:25 +0530

    Mutiple occurance of an element via ArrayList

--- RecursionArray/MultipleFindIndex.java
+++ RecursionArray/MultipleFindIndex.java
@@ -7,6 +7,8 @@ public class MultipleFindIndex {
         int[] arr ={1,2,3,4,4,5,6};
         ArrayList<Integer> ans = findIndex(arr, 4, 0, new ArrayList<>());
         System.out.println(ans);
+
+        System.out.println(findIndex2(arr, 4, 0));
     }
     static ArrayList<Integer> findIndex(int[] arr, int target, int index, ArrayList<Integer> list){
         if (index == arr.length) {
@@ -17,4 +19,18 @@ public class MultipleFindIndex {
         }
         return findIndex(arr, target, index+1, list);
     }
+    static ArrayList<Integer> findIndex2(int[] arr, int target, int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> ansForBelowCalls = findIndex2(arr, target, index+1);
+        list.addAll(ansForBelowCalls);
+        
+        return list;
+    }
+
 }
