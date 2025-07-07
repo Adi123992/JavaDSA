8a69f4a65479c41b87bfebd207c613e2a1aa8b62
addie <aditya.gumgaonkar9021@gmail.com>
2025-07-07 17:55:24 +0530

    Mobile pad numbers to characters conversion

---
+++ RecursionSubsetString/MobilePad.java
@@ -0,0 +1,40 @@
+package RecursionSubsetString;
+
+import java.util.ArrayList;
+
+public class MobilePad {
+    public static void main(String[] args) {
+        pad("", "12");
+        System.out.println();
+        System.out.println(pad2("", "12"));
+    }
+
+    static void pad(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.print(p + " ");
+            return;
+        }
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            pad(p + ch, up.substring(1));
+
+        }
+    }
+
+    static ArrayList<String> pad2(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        int digit = up.charAt(0) - '0';
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(pad2(p + ch, up.substring(1)));
+
+        }
+        return list;
+    }
+}
