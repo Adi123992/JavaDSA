c5ae2f96d5baab24e09f7a7265b96950aec7fb5a
addie <aditya.gumgaonkar9021@gmail.com>
2025-07-16 19:44:22 +0530

    Maze diagonal added

--- RecursionBackTracking/Maze.java
+++ RecursionBackTracking/Maze.java
@@ -1,8 +1,12 @@
 package RecursionBackTracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         System.out.println(count(3, 3));
+        System.out.println(path2("", 3, 3));
+        System.out.println(pathDiagonal("", 3, 3));
     }
 
     static int count(int r, int c) {
@@ -12,5 +16,55 @@ public class Maze {
         int left = count(r - 1, c);
         int right = count(r, c - 1);
         return left + right;
+    }
+
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            path("D", r - 1, c);
+        }
+        if (c > 1) {
+            path("R", r, c - 1);
+        }
+    }
+
+    static ArrayList<String> path2(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1) {
+            list.addAll(path2(p + "D", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(path2(p + "R", r, c - 1));
+        }
+        return list;
+    }
+
+    static ArrayList<String> pathDiagonal(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1 && c > 1) {
+            list.addAll(pathDiagonal(p + "D", r - 1, c - 1));
+        }
+        if (r > 1) {
+            list.addAll(pathDiagonal(p + "V", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathDiagonal(p + "H", r, c - 1));
+        }
+        return list;
     }
 }
