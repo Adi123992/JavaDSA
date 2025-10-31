c876b4c81f94618d8d1ac2ebddfd272ab3abe228
addie <aditya.gumgaonkar9021@gmail.com>
2025-10-31 21:09:36 +0530

    Generic final added

---
+++ OOPS/Generics/GenericMethod.java
@@ -0,0 +1,22 @@
+package OOPS.Generics;
+
+public class GenericMethod {
+    public static void main(String[] args) {
+        GenericMethod gm = new GenericMethod();
+        gm.open();
+        gm.printMe("Hello");
+        gm.printMeAgain(45.55f);
+    }
+
+    public void open() {
+        System.out.println("Opening the class soon");
+    }
+
+    public <T> void printMe(T num) {
+        System.out.println(num);
+    }
+
+    public <U extends Number> void printMeAgain(U num) {g
+        System.out.println(num);
+    }
+}
