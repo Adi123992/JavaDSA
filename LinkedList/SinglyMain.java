c17004a27dce9aefe5fce896dc8077e489d9aaf3
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-04 19:21:50 +0530

    Added Main method for SLL

--- LinkedList/SinglyMain.java
+++ LinkedList/SinglyMain.java
@@ -1,4 +1,15 @@
 package LinkedList;
 
 public class SinglyMain {
+    public static void main(String[] args) {
+        SinglyInsert l = new SinglyInsert(5);
+        l.insertFirst(3);
+        l.insertFirst(4);
+        l.insertFirst(5);
+        l.insertFirst(6);
+        l.insertFirst(37);
+        l.insertLast(77);
+        l.insertPosition(33, 3);
+        l.display();
+    }
 }
