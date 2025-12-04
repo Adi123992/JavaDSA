f75e4099c3a7926ce91ca84ee596324048dacaec
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-04 19:21:11 +0530

    Added SLL Insert

--- LinkedList/SinglyInsert.java
+++ LinkedList/SinglyInsert.java
@@ -1,26 +1,14 @@
 package LinkedList;
 
-public class LLIntro {
+public class SinglyInsert {
     private Node head;
     private Node tail;
     private int size;
 
-    public LLIntro(int size) {
+    public SinglyInsert(int size) {
         this.size = 0;
     }
 
-    public static void main(String[] args) {
-        LLIntro l = new LLIntro(5);
-        l.insertFirst(3);
-        l.insertFirst(4);
-        l.insertFirst(5);
-        l.insertFirst(6);
-        l.insertFirst(37);
-        l.insertLast(77);
-        l.insertPosition(33, 3);
-        l.display();
-
-    }
 
     //insertion at start of the LinkedList
     public void insertFirst(int value) {
