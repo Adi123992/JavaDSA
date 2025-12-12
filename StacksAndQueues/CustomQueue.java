d47eb189d01378f5f922b4ba6aa2cd5d5433be41
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-12 18:29:53 +0530

    Added Custom Queue program

---
+++ StacksAndQueues/CustomQueue.java
@@ -0,0 +1,67 @@
+package StacksAndQueues;
+
+public class CustomQueue {
+    private static final int DEFAULT_SIZE = 10;
+    protected int[] data;
+    int end = 0;
+
+    public CustomQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public CustomQueue() {
+        this(DEFAULT_SIZE);//points to the contructor with size parameter
+    }
+
+    public static void main(String[] args) throws Exception {
+        CustomQueue queue = new CustomQueue(5);
+        queue.insert(7);
+        queue.insert(8);
+        queue.insert(9);
+        queue.insert(10);
+        queue.insert(11);
+        queue.remove();
+        queue.display();
+
+    }
+
+    public boolean insert(int item) throws Exception {
+        if (isFull()) {
+            throw new Exception("Queue is full");
+        } else {
+            //insertion at rear i.e. at end
+            data[end] = item;
+            end++;
+            return true;
+        }
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        } else {
+            int removed = data[0];
+            //shift the elements after the removal from the front
+            for (int i = 1; i < end; i++) {
+                data[i - 1] = data[i];
+            }
+            end--;
+            return removed;
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < end; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public boolean isFull() {
+        return end == data.length;
+    }
+
+    public boolean isEmpty() {
+        return end == 0;
+    }
+}
