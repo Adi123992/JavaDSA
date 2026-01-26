c19be854687a50d3b5ab7469f4edec6b0e631fee
addie <aditya.gumgaonkar9021@gmail.com>
2026-01-26 21:22:12 +0530

    Added BST operations

--- BinaryTrees/Introduction/BST.java
+++ BinaryTrees/Introduction/BST.java
@@ -7,11 +7,13 @@ public class BST {
 
     }
 
+    //return the height of the node if node == null it returns -1 because the height at the leaf node is already 0
     public int getHeight(Node node) {
-        if (node == null) return 0;
+        if (node == null) return -1;
         return node.height;
     }
 
+    //inserts the value via helper function
     public void insert(int val) {
         root = insert(val, root);
     }
@@ -20,6 +22,21 @@ public class BST {
         return root == null;
     }
 
+    //first create a function and then a helper function within it to understand more
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    //helper function
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        //not only calculates for the root nodes but also checks the height diff of subtrees to avoid errors
+        return Math.abs(getHeight(node.left) - getHeight(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    //helper function of insert
     private Node insert(int val, Node node) {
         if (node == null) {
             Node n = new Node(val);
@@ -45,6 +62,27 @@ public class BST {
         for (int num : nums) {
             insert(num);
         }
+    }
+
+    //if the arr is sorted then take the middle of array and then build a tree
+    public void populateSorted(int[] nums) {
+        populateSorted(nums, 0, nums.length);
+    }
+
+    private void populateSorted(int[] nums, int start, int end) {
+        if (nums.length == 0) {
+            return;
+        }
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+
+        insert(nums[mid]);
+        //left arr
+        populateSorted(nums, start, mid);
+        //right arr
+        populateSorted(nums, mid + 1, end);
     }
 
     private void display(Node node, String details) {
